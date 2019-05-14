b8de7a55416e4d5f393f37a1a1c4043e6bd7b444
Steve <steve@australiansteve.com>
2019-05-14 09:20:33 -0300

    Add accordion and off canvas menu compone;5Dnts

--- assets/js/modules/foundation.js
+++ assets/js/modules/foundation.js
@@ -1,10 +1,14 @@
 import { Foundation } from 'foundation-sites/js/foundation.core'
 import { Tabs } from 'foundation-sites/js/foundation.tabs'
+import { AccordionMenu } from 'foundation-sites/js/foundation.accordionMenu'
+import { OffCanvas } from 'foundation-sites/js/foundation.offcanvas'
 
 function initFoundation() {
   // Include the Foundation Modules that we'll use in the app
   Foundation.plugin(Tabs, 'Tabs')
-
+  Foundation.plugin(AccordionMenu, 'AccordionMenu')
+  Foundation.plugin(OffCanvas, 'OffCanvas')
+  
   // Add jQuery to Foundation
   Foundation.addToJquery(jQuery)
 
