9763bb9912730dc26d14f0707c6a42d155597df6
Steve <steve@australiansteve.com>
2019-05-30 11:28:25 -0300

    Include plugins that will be pretty common among WCC projects

--- assets/js/modules/foundation.js
+++ assets/js/modules/foundation.js
@@ -2,12 +2,16 @@ import { Foundation } from 'foundation-sites/js/foundation.core'
 import { Tabs } from 'foundation-sites/js/foundation.tabs'
 import { AccordionMenu } from 'foundation-sites/js/foundation.accordionMenu'
 import { OffCanvas } from 'foundation-sites/js/foundation.offcanvas'
+import { Sticky } from 'foundation-sites/js/foundation.sticky'
+import { Equalizer } from 'foundation-sites/js/foundation.equalizer'
 
 function initFoundation() {
   // Include the Foundation Modules that we'll use in the app
   Foundation.plugin(Tabs, 'Tabs')
   Foundation.plugin(AccordionMenu, 'AccordionMenu')
   Foundation.plugin(OffCanvas, 'OffCanvas')
+  Foundation.plugin(Sticky, 'Sticky')
+  Foundation.plugin(Equalizer, 'Equalizer')
   
   // Add jQuery to Foundation
   Foundation.addToJquery(jQuery)
