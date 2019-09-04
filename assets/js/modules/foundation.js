e7cd6fba029b8fd3209bfd7936a33b2e66eb3472
Steve <steve@australiansteve.com>
2019-09-03 22:34:53 -0300

    Update to 6.5.3 and include Dropdown menus

--- assets/js/modules/foundation.js
+++ assets/js/modules/foundation.js
@@ -1,6 +1,7 @@
 import { Foundation } from 'foundation-sites/js/foundation.core'
 import { Tabs } from 'foundation-sites/js/foundation.tabs'
 import { AccordionMenu } from 'foundation-sites/js/foundation.accordionMenu'
+import { DropdownMenu } from 'foundation-sites/js/foundation.dropdownMenu'
 import { OffCanvas } from 'foundation-sites/js/foundation.offcanvas'
 import { Sticky } from 'foundation-sites/js/foundation.sticky'
 import { Equalizer } from 'foundation-sites/js/foundation.equalizer'
@@ -9,6 +10,7 @@ function initFoundation() {
   // Include the Foundation Modules that we'll use in the app
   Foundation.plugin(Tabs, 'Tabs')
   Foundation.plugin(AccordionMenu, 'AccordionMenu')
+  Foundation.plugin(DropdownMenu, 'DropdownMenu')
   Foundation.plugin(OffCanvas, 'OffCanvas')
   Foundation.plugin(Sticky, 'Sticky')
   Foundation.plugin(Equalizer, 'Equalizer')
